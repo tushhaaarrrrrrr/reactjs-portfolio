ca4dd3e275221d7c6d8401523c1e41c18c8d89e6
marrrkkk <definitelynotmark13@gmail.com>
2024-03-11 01:33:01 +0800

    feat: add new projects

--- src/components/Projects/Card.jsx
+++ src/components/Projects/Card.jsx
@@ -6,11 +6,11 @@ const Card = ({ title, content, img, tag, link }) => {
       <a
         href={link}
         target="_blank"
-        className="flex flex-col items-center justify-center mx-2 mb-10 text-lg transition-colors rounded-lg cursor-pointer hover:bg-white hover:bg-opacity-10 hover:drop-shadow-md"
+        className="flex flex-col items-center justify-center mx-3 mb-10 text-lg lg:h-[400px] transition-colors rounded-lg cursor-pointer hover:bg-white hover:bg-opacity-10 hover:drop-shadow-md"
       >
         <div className="pb-7">
           <div
-            className="w-[340px] h-[170px] m-5 rounded-lg bg-cover bg-white"
+            className="w-[280px] h-[170px] m-5 rounded-lg bg-cover bg-white"
             style={{ backgroundImage: `url(${img})` }}
           ></div>
           <div className="flex">
@@ -19,7 +19,7 @@ const Card = ({ title, content, img, tag, link }) => {
             </h1>
             <img src={arrow} width={20} />
           </div>
-          <p className="w-[340px] mb-2 ml-5 lg:ml-7 text-sm">{content}</p>
+          <p className="w-[280px] mb-2 ml-5 lg:ml-7 text-sm">{content}</p>
           <div className="flex">
             <p className="px-5 py-2 m-3 ml-5 lg:ml-7 text-sm text-black bg-white rounded-3xl">
               {tag}
--- src/components/Projects/Project.jsx
+++ src/components/Projects/Project.jsx
@@ -1,15 +1,16 @@
 import Card from "./Card";
 import snipe from "../../assets/snipe.webp";
 import ghibli from "../../assets/Ghibli.webp";
-import portfolio from "../../assets/portfolio.webp";
+import movie from "../../assets/movie.webp";
+import lastfm from "../../assets/lastfm.webp";
 
 const Project = () => {
   return (
-    <section className="slide-up">
+    <section className="slide-up flex flex-col items-center">
       <h1 className="flex justify-center mt-10 mb-5 text-2xl text-transparent this bg-clip-text bg-gradient-to-r from-blue-500 via-purple-500 to-pink-500 font-Inter">
         Projects
       </h1>
-      <div className="flex flex-col lg:items-start items-center justify-center lg:flex-row">
+      <div className="flex flex-col items-center justify-center lg:grid lg:grid-cols-2 lg:gap-4">
         <Card
           img={ghibli}
           title={"Ghibli"}
@@ -29,13 +30,21 @@ const Project = () => {
           link={"https://github.com/marrrkkk/SnipeBot"}
         />
         <Card
-          img={portfolio}
-          title={"Old Portfolio"}
+          img={movie}
+          title={"Movie Search App"}
           content={
-            "My old portfolio made with html and css, inspired by my favourite game, Red Dead Redemption 2."
+            "This project is a web application built using Next.js framework for server-side rendering and React.js"
           }
-          tag={"HTML + CSS"}
+          tag={"NextJS"}
           link={"https://marrrkkk.github.io/MarkLouie/"}
+        />
+        <Card
+          img={lastfm}
+          title={"Lastfm Scrobbler"}
+          content={
+            "Python script for automatic music scrobbling to Last.fm using the Last.fm API."
+          }
+          tag={"Python"}
         />
       </div>
     </section>
