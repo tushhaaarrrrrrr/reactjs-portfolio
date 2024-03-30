168f86a4eec0071949f30b418d94fe1b7d6f9926
Tushar Das <tushhaaarrrrrrr.das@gmail.com>
2024-03-31 04:24:34 +0530

    Final

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -18,19 +18,19 @@ const Home = () => {
     >
       <div className="w-80 md:w-[60%] lg:w-[35%]">
         <h1 className="this mt-10 text-5xl text-white lg:text-6xl lg:mt-0">
-          Hi, I'm{" "}
+          Full Stack{" "}
           <span className="text-transparent bg-clip-text bg-gradient-to-r from-blue-500 via-purple-500 to-pink-500">
-            Tushar
+            Developer
           </span>
         </h1>
         <h1 className="lg:w-full text-4xl text-white this lg:text-6xl">
           <span className="text-transparent bg-clip-text bg-gradient-to-r from-blue-500 via-purple-500 to-pink-500">
-            Full-Stack
+            Hi! I'm
           </span>{" "}
-          Developer
+          Tushar
         </h1>
         <p className="mt-3 text-left ">
-        Greetings! I'm Tushar Das, a multifaceted developer skilled in both front-end and back-end technologies. I specialize in HTML5, CSS, JavaScript, and frameworks such as Bootstrap, jQuery, SASS, React, and Redux, enabling me to design visually stunning and user-centric websites that prioritize seamless experiences. Beyond front-end development, I am proficient in Python for scientific computing using Jupyter Notebooks, NumPy, Pandas, and data visualization tools. Additionally, I possess extensive knowledge in machine learning, utilizing TensorFlow, Keras, NLP, reinforcement learning, and deep learning techniques to tackle complex data challenges.</p>
+        A developer skilled in both front-end and back-end technologies. I specialize in HTML5, CSS, JavaScript, and frameworks such as Bootstrap, jQuery, SASS, React, and Redux, enabling me to design visually stunning and user-centric websites that prioritize seamless experiences. Beyond front-end development, I am proficient in Python for scientific computing using Jupyter Notebooks, NumPy, Pandas, and data visualization tools. Additionally, I possess extensive knowledge in machine learning, utilizing TensorFlow, Keras, NLP, reinforcement learning, and deep learning techniques to tackle complex data challenges.</p>
         <ul className="flex mt-3 space-x-4">
           {icon.map((icon, i) => (
             <a href={link[i]} target="_blank" key={i}>
