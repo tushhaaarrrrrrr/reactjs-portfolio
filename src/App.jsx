ad9b1097fe0c37d7a4debb24079d47e0c1219da7
Tushar Das <tushhaaarrrrrrr.das@gmail.com>
2024-03-30 16:44:50 +0530

    Final

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,14 @@ import About from "./components/About/About";
 import Footer from "./components/Footer/Footer";
 import ScrollAnimationComponent from "./components/Animation";
 import AnimatedCursor from "react-animated-cursor";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 const App = () => {
   return (
     <>
       <AnimatedCursor color="255, 255, 255" innerSize={20} />
       <header>
+        <SpeedInsights/>
       </header>
       <main>
         <Home />
