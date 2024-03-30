97f0d0d21cefd3c9d7f2ae5f1ca6db97479297c0
Tushar Das <tushhaaarrrrrrr.das@gmail.com>
2024-03-31 00:21:00 +0530

    Final

--- src/components/About/Techstack.jsx
+++ src/components/About/Techstack.jsx
@@ -9,11 +9,11 @@ import python from "../../assets/python.svg";
 const Techstack = () => {
   const icons = [react, js, html, python, tailwind, nodejs, css];
   return (
-    <section className="flex justify-center mt-10 mb-40">
+    <section className="flex justify-center mt-10 mb-40 overflow-hidden">
       <ul className="flex flex-wrap space-x-3 lg:space-x-4">
         {icons.map((icon, i) => (
           <li key={i}>
-            <img src={icon} className="w-[40px] h-[40px] md:w-[55px] lg:w-[70px]" style={{ verticalAlign: 'middle' }} />
+            <img src={icon} className="w-[40px] md:w-[55px] lg:w-[70px]" style={{ verticalAlign: 'middle' }} />
           </li>
         ))}
       </ul>
